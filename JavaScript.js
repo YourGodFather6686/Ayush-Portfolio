d8a7f6d6eaab0f03d23ff83a15bc6b8d736b69a1
YourGodFather6686 <ayushdanapur@gmail.com>
2024-12-08 01:31:47 +0530

    Not Applied Yet Into The Portfolio

---
+++ JavaScript.js
@@ -0,0 +1,14 @@
+/*
+const toggleMenus = document.querySelectorAll('.toggle-menu');
+
+toggleMenus.forEach(menu => {
+    menu.addEventListener('click', function () {
+
+        toggleMenus.forEach(m => {
+            if (m !== this) m.classList.remove('active');
+        });
+
+        this.classList.toggle('active');
+    });
+}) 
+    */
